67b4b57de7a05f4c1e492d41fdafa2dc8a1786f4
Suryam <suryam.srivastava@sap.com>
2022-07-14 23:20:59 +0530

    Arrays

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30};
+        System.out.println(Arrays.toString(numbers));
+    }
+}
