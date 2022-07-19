0867ee25771846b843345fb93c3039836fe7c138
Suryam <suryam.srivastava@sap.com>
2022-07-19 22:37:54 +0530

    Reverse a String using Stack

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
 
-        MyStack myStack = new MyStack();
-        myStack.reverseString("abcde");
+        //M implementation for Reversing the string
+        StringReverser reverser = new StringReverser();
+        var result = reverser.reverse("input");
+        System.out.println(result);
+
+        
+        // MyStack myStack = new MyStack();
+        // myStack.reverseString("abcde");
 //Stack
         // Stack<Integer> stack = new Stack<>();
         // stack.push(10);
