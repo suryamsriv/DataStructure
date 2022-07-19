fa743392bdc27bb157d0d5d64b0bd85f3f44bf71
Suryam <suryam.srivastava@sap.com>
2022-07-19 22:22:43 +0530

    Stack

--- Main.java
+++ Main.java
@@ -17,17 +17,6 @@ public class Main {
         // System.out.println(stack);
         // top = stack.peek();
         // System.out.println(top);
-//Stack
-        Stack<Integer> stack = new Stack<>();
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-        System.out.println(stack);
-        var top = stack.pop();
-        System.out.println(top);
-        System.out.println(stack);
-        top = stack.peek();
-        System.out.println(top);
 
 //Linked List
         // LinkedList list = new LinkedList();
