7b28138902e2c53b19db3296962910d10761413b
Suryam <suryam.srivastava@sap.com>
2022-07-31 22:25:02 +0530

    Minimum value in tree

--- Main.java
+++ Main.java
@@ -6,17 +6,18 @@ public class Main {
         public static void main(String[] args) {
 
                 BinaryTree tree = new BinaryTree();
-                // tree.insert(7);
-                // tree.insert(4);
-                // tree.insert(9);
-                // tree.insert(1);
-                // tree.insert(6);
-                // tree.insert(8);
-                // tree.insert(10);
+                tree.insert(7);
+                tree.insert(4);
+                tree.insert(9);
+                tree.insert(1);
+                tree.insert(6);
+                tree.insert(8);
+                tree.insert(10);
                 // System.out.println(tree.find(1));
                 tree.traversePostOrder();
 
                 System.out.println("height: " + tree.height());
+                System.out.println("min: " + tree.min());
 
                 // HashMap implementation
                 // MyHashTable table = new MyHashTable();
