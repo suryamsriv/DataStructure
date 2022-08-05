e793a20e9936c062fcfc0481ca9947f3e5822b86
Suryam <suryam.srivastava@sap.com>
2022-08-06 01:18:07 +0530

    Priority queue with Heap

--- Main.java
+++ Main.java
@@ -5,14 +5,14 @@ import java.util.Queue;
 public class Main {
         public static void main(String[] args) {
 
-                var heap = new Heap();
-                heap.insert(10);
-                heap.insert(5);
-                heap.insert(17);
-                heap.insert(4);
-                heap.insert(22);
-                heap.remove();
-                System.out.println("Done");
+                // var heap = new Heap();
+                // heap.insert(10);
+                // heap.insert(5);
+                // heap.insert(17);
+                // heap.insert(4);
+                // heap.insert(22);
+                // heap.remove();
+                // System.out.println("Done");
 
                 // AVLTree tree = new AVLTree();
                 // tree.insert(30);
---
+++ PriorityQueueWithHeap.java
@@ -0,0 +1,15 @@
+public class PriorityQueueWithHeap {
+    private Heap heap = new Heap();
+
+    public void add(int item) {
+        heap.insert(item);
+    }
+
+    public int remove() {
+        return heap.remove();
+    }
+
+    public boolean isEmpty() {
+        return heap.isEmpty();
+    }
+}
