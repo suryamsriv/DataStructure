2706927d80b6dd37cccc570c2197505b8ec90239
Suryam <suryam.srivastava@sap.com>
2022-08-06 01:31:41 +0530

    Heapify

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ import java.util.Queue;
 public class Main {
         public static void main(String[] args) {
 
+                int numbers[] = { 5, 3, 8, 4, 1, 2 };
+                MaxHeap.heapify(numbers);
+                System.out.println(Arrays.toString(numbers));
+
                 // var heap = new Heap();
                 // heap.insert(10);
                 // heap.insert(5);
