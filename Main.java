00958f05b81f96e593b170b432ce6650468a777e
Suryam <suryam.srivastava@sap.com>
2022-08-19 00:48:53 +0530

    Bubble Sort

--- Main.java
+++ Main.java
@@ -6,18 +6,22 @@ import java.util.Queue;
 public class Main {
         public static void main(String[] args) {
 
-                var graph = new WeightedGraph();
-                graph.addNode("A");
-                graph.addNode("B");
-                graph.addNode("C");
-                graph.addNode("D");
-                graph.addEdge("A", "B", 3);
-                graph.addEdge("B", "D", 4);
-                graph.addEdge("C", "D", 5);
-                graph.addEdge("A", "C", 1);
-                graph.addEdge("B", "C", 2);
-                var tree = graph.getMinimumSpanningTree();
-                tree.print();
+                int[] numbers = { 7, 3, 1, 4, 6, 2, 3 };
+                var sorter = new BubbleSort();
+                sorter.sort(numbers);
+                System.out.println(Arrays.toString(numbers));
+                // var graph = new WeightedGraph();
+                // graph.addNode("A");
+                // graph.addNode("B");
+                // graph.addNode("C");
+                // graph.addNode("D");
+                // graph.addEdge("A", "B", 3);
+                // graph.addEdge("B", "D", 4);
+                // graph.addEdge("C", "D", 5);
+                // graph.addEdge("A", "C", 1);
+                // graph.addEdge("B", "C", 2);
+                // var tree = graph.getMinimumSpanningTree();
+                // tree.print();
 
                 // var path = graph.getShortestPath("A", "C");
                 // System.out.println(path);
