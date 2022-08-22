108061c8fa80996998013cdbfebd82f41e3a3da4
Suryam <suryam.srivastava@sap.com>
2022-08-22 23:53:46 +0530

    Bucket Sort

---
+++ BucketSort.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BucketSort {
+    public void sort(int[] array, int numberOfBuckets) {
+
+        var i = 0;
+        for (var bucket : createBucket(array, numberOfBuckets)) {
+            Collections.sort(bucket);
+            for (var item : bucket)
+                array[i++] = item;
+
+        }
+    }
+
+    private List<List<Integer>> createBucket(int array[], int numberOfBuckets) {
+        List<List<Integer>> buckets = new ArrayList<>();
+
+        for (var i = 0; i < numberOfBuckets; i++)
+            buckets.add(new ArrayList<>());
+
+        for (var item : array)
+            buckets.get(item / numberOfBuckets).add(item);
+
+        return buckets;
+    }
+}
