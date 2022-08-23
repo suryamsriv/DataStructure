635b24300c5c5ab63c31f585a53a5cc5afeaba81
Suryam <suryam.srivastava@sap.com>
2022-08-23 22:35:19 +0530

    Linear Search

---
+++ Search.java
@@ -0,0 +1,9 @@
+public class Search {
+    public int linearSearch(int[] array, int target) {
+        for (var i = 0; i < array.length; i++)
+            if (array[i] == target)
+                return i;
+
+        return -1;
+    }
+}
