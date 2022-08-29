3a2f063b0d0452f4fe63143589e3a3bfab1db59e
Suryam <suryam.srivastava@sap.com>
2022-08-30 00:29:10 +0530

    String reversal

--- StringUtils.java
+++ StringUtils.java
@@ -1,5 +1,7 @@
 public class StringUtils {
     public static int countVowels(String str) {
+        if (str == null)
+            return 0;
         int count = 0;
         String vowels = "aeiou";
         for (var ch : str.toLowerCase().toCharArray())
@@ -7,5 +9,13 @@ public class StringUtils {
                 count++;
 
         return count;
+    }
+
+    public static String reverse(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (var i = str.length(); i >= 0; i--)
+            reversed.append(str.charAt(i));
+
+        return reversed.toString();
     }
 }
