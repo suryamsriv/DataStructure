68dbdbe8c1481922746df68a4407935ce6f1da75
Suryam <suryam.srivastava@sap.com>
2022-09-05 19:13:33 +0530

    Binary Tree

--- BinaryTree2.java
+++ BinaryTree2.java
@@ -52,4 +52,58 @@ public class BinaryTree2 {
         return false;
     }
 
+    public void traversePreOrder() {
+        traversePreOrder(root);
+    }
+
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
+        System.out.println(root.value);
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+    }
+
+    public void traverseInOrder() {
+        traverseInOrder(root);
+    }
+
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
+        traversePreOrder(root.leftChild);
+        System.out.println(root.value);
+        traversePreOrder(root.rightChild);
+    }
+
+    public void traversePostOrder() {
+        traversePostOrder(root);
+    }
+
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
+        traversePreOrder(root.leftChild);
+        traversePreOrder(root.rightChild);
+        System.out.println(root.value);
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node root) {
+        if (root.leftChild == null && root.rightChild == null)
+            return 0;
+
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    private int min(Node root) {
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+
+        return Math.min(Math.min(left, right), root.value);
+    }
+
 }
